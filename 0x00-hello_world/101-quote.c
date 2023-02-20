35e5f5ee658cf47147d718d01a8c1ed3611d7684
Jessica Ogu <ogujessica3@gmail.com>
2023-02-20 14:00:56 -0800

    task final

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,9 +3,10 @@
 
 /**
  * main - A c program that prints a line to the standard error
- *
- * Return: 1 
+ * Return: 1
  */
+
+
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
