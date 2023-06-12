7b760dfab0435b143e422ce6dae37b6a3471a9fe
JessicaChideraOgu <ogujessica3@gmail.com>
2023-06-12 23:28:13 +0100

    Added task one file

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - The actual program
+ * @argc: The number of arguments
+ * @argv: The array of strings
+ * return - Returns 0.
+ *
+ */
+int main(int argc, char *argv[]__attribute__((unused)))
+{
+	printf("argc = %d\n", argc);
+	return (0);
+}
