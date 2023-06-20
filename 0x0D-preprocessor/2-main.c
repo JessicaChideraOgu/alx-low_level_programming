4baf23b3ba977c8080d7161e4e85606f709046ac
JessicaChideraOgu <ogujessica3@gmail.com>
2023-06-20 03:26:20 +0100

    Added Task two file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/*
+ * main - program that prints the file name.
+ * Return: Returns 0 at success.
+ */
+int main(void)
+{
+	printf("The file name is: %s\n", __FILE__);
+	return (0);
+}
